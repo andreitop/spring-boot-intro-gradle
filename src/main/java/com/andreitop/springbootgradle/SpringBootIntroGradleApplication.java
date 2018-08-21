04e6263f1092265adb32dcc64e99ba5b3df413af
Andrei Dremov <andrei_dremov@epam.com>
2018-08-21 16:20:44 +0300

    Initial commit

---
+++ src/main/java/com/andreitop/springbootgradle/SpringBootIntroGradleApplication.java
@@ -0,0 +1,12 @@
+package com.andreitop.springbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIntroGradleApplication.class, args);
+    }
+}
