0fc4c9d64072880fb955b40b0b7d7faefc73a339
Andrei Dremov <andrei_dremov@epam.com>
2018-08-21 16:46:25 +0300

    Step 3 | Create traditional deployment war + embedded mode

--- src/main/java/com/andreitop/springbootgradle/SpringBootIntroGradleApplication.java
+++ src/main/java/com/andreitop/springbootgradle/SpringBootIntroGradleApplication.java
@@ -2,11 +2,29 @@ package com.andreitop.springbootgradle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SpringBootIntroGradleApplication {
+public class SpringBootIntroGradleApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(SpringBootIntroGradleApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIntroGradleApplication.class, args);
+    }
+
+    @RestController
+    class SimpleController {
+
+        @RequestMapping("/call")
+        String callMe() {
+            return "Hello, friend!";
+        }
     }
 }
